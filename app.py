b070fdfd55c6f64a4c8deb75c486e4688be7c5c1
allan chiu <56469564+allanchiu@users.noreply.github.com>
2025-05-21 18:51:59 +0800

    conversation-list

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ apiclient = PureCloudPlatformClientV2.api_client.ApiClient()
 apiclient.host = ENVIRONMENT
 apiclient.get_client_credentials_token(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 
+# Create Conversation API Instance
+conversationApi = PureCloudPlatformClientV2.ConversationsApi(apiclient)
+
 # Create Rec API Instance
 recordApi = PureCloudPlatformClientV2.RecordingApi(apiclient)
 
@@ -24,8 +27,36 @@ def index():
     return "Hello World!"
 
 
-@app.route("/get_recordings")
-def get_recordings():
+@app.route("/conversations")
+def get_conversations():
+    body = PureCloudPlatformClientV2.ConversationQuery()
+
+    segmentFilter = PureCloudPlatformClientV2.SegmentDetailQueryFilter()
+
+    predicte01 = PureCloudPlatformClientV2.SegmentDetailQueryPredicate()
+    predicte01.type = "dimension"
+    predicte01.dimension = "direction"
+    predicte01.operator = "matches"
+    predicte01.value = "inbound"
+    segmentFilter.predicates = [predicte01]
+    segmentFilter.type = "or"
+    body.segment_filters = [segmentFilter]
+    body.interval = "2025-04-10T00:00:00.000Z/2025-04-10T23:59:59.999Z"
+
+    try:
+        api_response = conversationApi.post_analytics_conversations_details_query(body)
+        response = api_response.to_dict()
+        conversations = response["conversations"]
+        total_hits = response["total_hits"]
+        for i in range(0, total_hits):
+            print(response["conversations"][i]["conversation_id"])
+        return(conversations)
+    except ApiException as e:
+        print("Exception when calling ConversationApi %s\n" % e)
+
+
+@app.route("/play_recording")
+def pay_recording():
     conversation_id = "b938ff7b-d1d3-4e43-9aaf-f8287c839ed8"  # str | Conversation ID
     max_wait_ms = 5000  # int | The maximum number of milliseconds to wait for the recording to be ready. Must be a positive value. (optional) (default to 5000)
     format_id = "WAV"  # str | The desired media format. Valid values:WAV,WEBM,WAV_ULAW,OGG_VORBIS,OGG_OPUS,MP3,NONE. (optional) (default to 'WEBM')
